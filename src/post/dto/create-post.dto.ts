49a153a79bc849bcb301e3e01b45e9dc6c058471
Berezovskiy Andriy <berezovskiy.andriy.it@gmail.com>
2022-06-14 20:39:44 +0300

    -

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePostDto {
